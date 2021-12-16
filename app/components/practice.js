bb412ecabac71692b7fecfc3b1533ea15075dbb3
DashDeipayan <deipayan.dash8@gmail.com>
2021-12-16 08:13:18 +0530

    FadeIn,FadeOut, inserted&removed Sprite

--- app/components/practice.js
+++ app/components/practice.js
@@ -1,16 +1,28 @@
+/* eslint-disable require-yield */
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import fade from 'ember-animated/transitions/fade';
+//import fade from 'ember-animated/transitions/fade';
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
 
 export default class ClothesComponent extends Component {
   @tracked
   value = false;
 
-  transition = fade;
-
   @action
   toggle() {
     this.value = !this.value;
+  }
+
+  //Declaring custom fade animation using es-generators
+  //transition = fade;
+
+  *transition({ insertedSprites, removedSprites }) {
+    for (let sprite of insertedSprites) {
+      fadeIn(sprite);
+    }
+    for (let sprite of removedSprites) {
+      fadeOut(sprite);
+    }
   }
 }
